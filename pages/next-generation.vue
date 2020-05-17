732608af5bc65593ebc4b1e68acf2d7e9865d880
kidzki <kidzki@outlook.de>
2020-05-17 21:30:38 +0200

    CHG: fixed scrollingproblem on mobile

--- pages/next-generation.vue
+++ pages/next-generation.vue
@@ -5,34 +5,37 @@
     <div class="page__wrap">
       <label for="menu-switch" id="menu-toggle" class="menu__toggle"></label>
       <div class="row">
-        <div class="col-12">
-          <div class="soundboard">
-            <div class="soundboard__header">
-              <h1>SvenPanel 3.0</h1>
-              <h3>Next-Generation</h3>
-            </div>
-            <div class="row">
-              <player v-for="(sound, index) in sounds"
+        <div class="soundboard">
+          <div class="soundboard__header">
+            <h1>SvenPanel 3.0</h1>
+            <h3>Next-Generation</h3>
+          </div>
+          <div class="row">
+            <player
+              v-for="(sound, index) in sounds"
               :key="index"
               :soundfile="sound.file"
-              :soundname="sound.name"/>
-            </div>
+              :soundname="sound.name"
+            />
           </div>
         </div>
       </div>
     </div>
-    <p class="made">This PWA is made with ❤ and hosted on <a href="https://github.com/kidzki/theshrine_v3">Github</a></p>
+    <p class="made">
+      This PWA is made with ❤ and hosted on
+      <a href="https://github.com/kidzki/theshrine_v3">Github</a>
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Burgermenu from "~/components/burgermenu.vue";
-import Player from '~/components/player.vue';
-import soundmap from '~/static/json/next-generation.json';
+import Player from "~/components/player.vue";
+import soundmap from "~/static/json/next-generation.json";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     Burgermenu,
     Player
@@ -40,7 +43,7 @@ export default {
   computed: {
     sounds() {
       return soundmap.map(item => item);
-    },
-  },
+    }
+  }
 };
 </script>
