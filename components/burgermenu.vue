fff37cc1128543498aa98570b5f2c2dbdb94b8ab
kidzki <kidzki@outlook.de>
2020-05-17 21:47:43 +0200

    CHG: updated url handling

--- components/burgermenu.vue
+++ components/burgermenu.vue
@@ -2,16 +2,8 @@
   <nav class="menu" role="navigation">
     <div class="menu__brand">SvenPanel 3.0</div>
     <ul>
-      <li><nuxt-link to="/" prefetch>Classic</nuxt-link></li>
-      <li><nuxt-link to="/next-generation" prefetch>Next-Generation</nuxt-link></li>
+      <li v-if="this.$route.path !== '/'"><nuxt-link to="/" prefetch>Classic</nuxt-link></li>
+      <li v-if="this.$route.path !== '/next-generation'"><nuxt-link to="/next-generation" prefetch>Next-Generation</nuxt-link></li>
     </ul>
   </nav>
 </template>
-
-<script>
-export default {
-  beforeCreate() {
-    console.log(this.$route.path === '/next-generation');
-  }
-}
-</script>
