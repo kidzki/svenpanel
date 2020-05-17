5845ab4531c49e9dac119209c6dac6972d799e00
kidzki <kidzki@outlook.de>
2020-05-17 21:46:02 +0200

    CHG: added prefetch to url

--- components/burgermenu.vue
+++ components/burgermenu.vue
@@ -2,8 +2,16 @@
   <nav class="menu" role="navigation">
     <div class="menu__brand">SvenPanel 3.0</div>
     <ul>
-      <li><nuxt-link to="/">Classic</nuxt-link></li>
-      <li><nuxt-link to="/next-generation">Next-Generation</nuxt-link></li>
+      <li><nuxt-link to="/" prefetch>Classic</nuxt-link></li>
+      <li><nuxt-link to="/next-generation" prefetch>Next-Generation</nuxt-link></li>
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  beforeCreate() {
+    console.log(this.$route.path === '/next-generation');
+  }
+}
+</script>
