824dd54b5ec7467129eb0067bb84e4a586f643d8
crypt32dll <sf_fabi@hotmail.de>
2020-05-17 20:29:10 +0200

    [UPDATE] dark background for menu

--- components/burgermenu.vue
+++ components/burgermenu.vue
@@ -1,5 +1,6 @@
 <template>
   <nav class="menu" role="navigation">
+    <div class="menu__brand">SvenPanel 3.0</div>
     <ul>
       <li><nuxt-link to="/">Classic</nuxt-link></li>
       <li><nuxt-link to="/next-generation">Next-Generation</nuxt-link></li>
